8759599af1f3ba0985f72623fe805efed7f2cb60
Matan Gino <matangino@Matans-MacBook-Pro.local>
2023-09-05 17:34:45 +0300

    factorial function

--- program.py
+++ program.py
@@ -1,8 +1,26 @@
 
 
+import math
+
+def factorial(num: int) -> int:
+    """
+    function that returns the factorial value of a number
+    input: int-> number
+    return: the factorial result
+    """
+    if type(num)!= int:
+        return None
+    
+    return math.factorial(num)
+
+
 def main():
     my_sum = sum([num for num in range(1, 101)])
     print(my_sum)
+
+    print(factorial(6.5))
+    print(factorial(7))
+    print(factorial(8))
 
 
 if __name__ == "__main__":
