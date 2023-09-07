bcdfdf1064ce378d1cfc15c091b172674422aafd
Matan Gino <matangino@Matans-MacBook-Pro.local>
2023-09-07 13:26:22 +0300

    added_files

---
+++ persons/person/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Person(models.Model):
+    name = models.CharField(max_length=20)
+    id = models.IntegerField(primary_key=True)
+    date_of_birth = models.DateField()
+    city = models.CharField(max_length= 100)
+
+
+class Parent(Person):
+    work_place = models.CharField(max_length=20)
+    salary = models.DecimalField(max_digits=8, decimal_places=2)
+    kids = models.ManyToManyField('Person', blank=True, null=True,  related_name='parents')
