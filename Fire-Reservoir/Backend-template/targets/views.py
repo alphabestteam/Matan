aa9c6e29e3c7b1197d4094e71ed052ac6a843c3e
Matan Gino <matangino@Matans-MacBook-Pro.local>
2023-10-16 16:07:20 +0300

    final

--- Fire-Reservoir/Backend-template/targets/views.py
+++ Fire-Reservoir/Backend-template/targets/views.py
@@ -13,20 +13,11 @@ def add_target(request):
     # Implement here an add function
     if request.method == "POST":
         data = json.loads(request.body.decode("utf-8"))
-        target = Target(
-            name=data["name"],
-            attack_priority=data["attack_priority"],
-            longitude=data["longitude"],
-            latitude=data["latitude"],
-            enemy_organization=data["enemy_organization"],
-            target_goal=data["target_goal"],
-            was_target_destroyed=data["was_target_destroyed"],
-            target_id=data["target_id"]
-        )
+        target = TargetSerializer(data)
         serializer = TargetSerializer(data=data)
         if serializer.is_valid():
             target.save()
-            return JsonResponse({"message": "Target created successfully"})
+            return JsonResponse({"message": "Target created successfully"}, status=200)
         else:
             return JsonResponse({"message": "Invalid request method"}, status=400)
 
@@ -41,14 +32,7 @@ def update_target(request):
             serializer = TargetSerializer(target, data=request_data)
             
             if serializer.is_valid():
-                target.name= request_data['name']
-                target.attack_priority = request_data["attack_priority"]
-                target.longitude = request_data["longitude"]
-                target.latitude = request_data['latitude']
-                target.enemy_organization = request_data["enemy_organization"]
-                target.target_goal = request_data["target_goal"]
-                target.was_target_destroyed = request_data["was_target_destroyed"]
-                target.save()
+                serializer.save()
                 return JsonResponse(serializer.data, safe=False)
             
             return JsonResponse(serializer.errors, status=400)
